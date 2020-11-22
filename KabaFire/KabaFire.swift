374246ce096c7c1ee7965fd7e1f739327967cfb6
kaba <taikikabaya@gmail.com>
2020-11-22 23:54:56 +0900

    httpRequestのコードを共通化した

--- KabaFire/KabaFire.swift
+++ KabaFire/KabaFire.swift
@@ -13,22 +13,40 @@ public class KabaFire {
     public static let name = "kabaya"
     
     public static func get<T: Codable>(url: String, model: T.Type, completion: @escaping(T) -> Void) {
-        let task = URLSession.shared.dataTask(with: URL(string: url)!) {(data, response, error) in
-            guard let data = data else { return }
+            let uri = URL(string: url)!
+            let request = URLRequest(url: uri)
             
+            self.request(uri: uri, request: request, completion: { value in
+                completion(value)
+            })
+    }
+    
+    private static func request<T: Codable>(uri: URL, request: URLRequest, completion: @escaping(T) -> Void) {
+        let task = URLSession.shared.dataTask(with: uri) {(data, response, error) in
+            guard let data = data else { return }
             do {
                 let value: T = try JSONDecoder().decode(T.self, from: data)
-                completion(value)
+                    completion(value)
             } catch {
                 // noop
             }
         }
-
         task.resume()
     }
     
-//    public static func post<T: Codable>() {
+//    public static func post<T: Codable>(url: String) -> Void {
+//        let uri = URL(string: url)
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST" // この一行がないとGetになる
 //
+//        let task = URLSession.shared.dataTask(with: uri) {(data, response, error) in
+//        guard let data = data else { return }
+//
+//            let value: T = try JSONDecoder().decode(T.self, from: data)
+//            completion(value)
+//        } catch {
+//            // noop
+//        }
 //    }
 //
 //    public static func put() {
