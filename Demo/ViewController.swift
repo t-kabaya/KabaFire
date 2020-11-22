346c09b3679509ae4697cbf128af32e6e493c2a1
kaba <taikikabaya@gmail.com>
2020-11-22 23:34:53 +0900

    不要なコードを削除

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import KabaFire
 
-struct People: Codable {
+struct Person: Codable {
     let name: String?
 }
 
@@ -18,36 +18,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let url = "https://swapi.dev/api/people/1/"
-        KabaFire.get(url: url, model: People.self, completion: { response in
-            print(response)
+        KabaFire.get(url: url, model: Person.self, completion: { response in
+            let person: Person = response
+            print(person)
         })
         
-        let str = """
-            {"name": "Robert", "age" : 35}
-            """
-
-        let data = str.data(using: .utf8)
-        let jsonParser = JSONParser(data: data!, model: TestModel.self)
     }
-}
-
-class JSONParser: NSObject {
-    let newJSONDecoder : JSONDecoder
-    let data : Data
-
-    init<T: Codable>(data: Data, model: T.Type) {
-        self.newJSONDecoder = JSONDecoder()
-        do {
-        let result = try self.newJSONDecoder.decode(model.self, from: data)
-            print(result)
-        } catch let err {
-            print(err.localizedDescription)
-        }
-        self.data = data
-    }
-}
-
-struct TestModel: Codable {
-    let name: String
-    let age: Int
 }
