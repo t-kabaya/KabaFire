d46c9e83457970e275493752fb4a99283bf7e58a
kaba <taikikabaya@gmail.com>
2020-11-23 19:28:43 +0900

    headerを付与したが、動作未確認

--- KabaFire/KabaFire.swift
+++ KabaFire/KabaFire.swift
@@ -12,36 +12,43 @@ import UIKit
 public class KabaFire {
     public static let name = "kabaya"
     
-    public static func get<T: Codable>(url: String, model: T.Type, completion: @escaping(T) -> Void) {
+    public static func get<T: Codable>(url: String, model: T.Type, header: [String : String] = [:], completion: @escaping(T) -> Void) {
         self.request(url: url, method: "GET", completion: { value in
                 completion(value)
         })
     }
     
     // TODO: bodyに未対応
-    public static func post<T: Codable>(url: String, model: T.Type, completion: @escaping(T) -> Void) -> Void {
-        self.request(url: url, method: "POST", completion: { value in
+    public static func post<T: Codable>(url: String, model: T.Type, header: [String : String] = [:], completion: @escaping(T) -> Void) -> Void {
+        self.request(url: url, method: "POST", header: header, completion: { value in
             completion(value)
         })
     }
     
     // TODO: bodyに未対応
-    public static func put<T: Codable>(url: String, model: T.Type, completion: @escaping(T) -> Void) -> Void {
+    public static func put<T: Codable>(url: String, model: T.Type, header: [String : String] = [:], completion: @escaping(T) -> Void) -> Void {
         self.request(url: url, method: "PUT", completion: { value in
             completion(value)
         })
     }
     
-    public static func delete<T: Codable>(url: String, model: T.Type, completion: @escaping(T) -> Void) -> Void {
+    public static func delete<T: Codable>(url: String, model: T.Type, header: [String : String] = [:], completion: @escaping(T) -> Void) -> Void {
         self.request(url: url, method: "DELETE", completion: { value in
             completion(value)
         })
     }
     
-    private static func request<T: Codable>(url: String, method: String, completion: @escaping(T) -> Void) {
+    // 現状headerには、Stringしかセット出来ない。
+    private static func request<T: Codable>(url: String, method: String, header: [String : String] = [:], completion: @escaping(T) -> Void) {
         let uri = URL(string: url)!
+        
         var request = URLRequest(url: uri)
         request.httpMethod = method
+        
+        // headerを付与
+        for (key, value) in header {
+            request.setValue(key, forHTTPHeaderField: value)
+        }
 
         let task = URLSession.shared.dataTask(with: uri) {(data, response, error) in
             guard let data = data else { return }
